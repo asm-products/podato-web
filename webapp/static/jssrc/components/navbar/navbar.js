150882d8c04a78bd25a2a1ed2b6ec68bdc8af13f
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-09 23:38:30 +0200

    add search input to navbar.

--- webapp/static/jssrc/components/navbar/navbar.js
+++ webapp/static/jssrc/components/navbar/navbar.js
@@ -1,4 +1,5 @@
 const React = require("react");
+const Link = require("react-router").Link;
 
 const Navbar = React.createClass({
     render(){
@@ -6,9 +7,8 @@ const Navbar = React.createClass({
             <nav className="fixed top-0 left-0 right-0 bg-red white px4" style={{height:"2.5rem"}}>
                 <div className="container flex flex-stretch" style={{height:"100%"}}>
                     <img src="/img/logo.png" style={{height:"100%"}}/>
-                    <a href="/" className="button button-red">Home</a>
-                    <a href="/" className="button button-red">Explore</a>
-                    <a href="/" className="button button-red">Settings</a>
+                    <Link to="home" className="button button-red">Home</Link>
+                    <div style={{padding:"0.5rem"}}><input type="search" name="search" style={{height:"1.5rem"}} /></div>
                 </div>
             </nav>
         )
