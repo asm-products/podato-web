b135a7a646074493982652d4cedc08620597cd86
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-09 21:37:45 +0200

    add logo to nav.

--- webapp/static/jssrc/components/navbar/navbar.js
+++ webapp/static/jssrc/components/navbar/navbar.js
@@ -3,8 +3,9 @@ const React = require("react");
 const Navbar = React.createClass({
     render(){
         return (
-            <nav className="fixed top-0 left-0 right-0 bg-red white px4 flex flex-align-center">
-                <div className="container">
+            <nav className="fixed top-0 left-0 right-0 bg-red white px4" style={{height:"2.5rem"}}>
+                <div className="container flex flex-stretch" style={{height:"100%"}}>
+                    <img src="/img/logo.png" style={{height:"100%"}}/>
                     <a href="/" className="button button-red">Home</a>
                     <a href="/" className="button button-red">Explore</a>
                     <a href="/" className="button button-red">Settings</a>
