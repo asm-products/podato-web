bef638fe247c1a40328a028cd6d4c71fab83cc4f
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-16 21:41:16 +0200

    added SubscriptionsStore

---
+++ webapp/static/jssrc/stores/subscriptions-store.js
@@ -0,0 +1,26 @@
+const mcfly = require("../mcfly");
+const constants = require("../constants");
+
+var subscriptions = {}
+var fetching = []
+
+const SubscriptionsStore = mcfly.createStore({
+    getSubscriptions(userId){return subscriptions[userId]},
+    isFetchingSubscriptions(userId){return (fetching.indexOf(userId) >= 0)}
+}, function(data){
+    switch(data.actionType){
+        case constants.actionTypes.SUBSCRIPTIONS_FETCHED:
+            subscriptions[data.userId] = data.subscriptions;
+            if (fetching.indexOf(data.userId) >= 0){
+                fetching.splice(fetching.indexOf(data.userId), 1);
+            }
+            break;
+        case constants.actionTypes.FETCHING_SUBSCRIPTIONS:
+            fetching.push(data.userId);
+        default:
+            return
+    }
+    SubscriptionsStore.emitChange();
+});
+
+module.exports = SubscriptionsStore;
