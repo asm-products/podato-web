175dff43b5fcc6f18834bbd6d2d9e0d7bce84f81
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-16 19:32:26 +0200

    added fetchSubscriptions action.

--- webapp/static/jssrc/actions/podcast-actions.js
+++ webapp/static/jssrc/actions/podcast-actions.js
@@ -6,12 +6,13 @@ const AuthActions = mcfly.createActions({
     fetchPodcast(podcastId){
         console.log("fetchPodcast action called.");
         return new Promise((resolve, reject) => {
-            api.loaded.then(function(){
-                api.podcasts.getPodcast({podcastId: podcastId}, function(resp){
+            api.loaded.then(() => {
+                api.podcasts.getPodcast({podcastId: podcastId}, (resp) => {
                     console.log("getpodcast response for " + podcastId);
                     console.log(resp);
                     if(resp.status !== 200){
                         reject(resp.statusText);
+                        return
                     }
                     resolve({
                         actionType: constants.actionTypes.PODCAST_FETCHED,
@@ -20,6 +21,30 @@ const AuthActions = mcfly.createActions({
                 });
             });
         });
+    },
+    fetchSubscriptions(userId){
+        userId = userId || "me"
+        return new Promise((resolve, reject) => {
+            api.loaded.then(() => {
+                api.users.getSubscriptions({userId: userId}, (resp) => {
+                    if(resp.status !== 200){
+                        reject(resp.statusText);
+                        return
+                    }
+                    resolve({
+                        actionType: constants.actionTypes.SUBSCRIPTIONS_FETCHED,
+                        subscriptions: resp.obj,
+                        userId: userId
+                    })
+                })
+            });
+        });
+    },
+    fetchingSubscriptions(userId){
+        return {
+            actionType: constants.actionTypes.FETCHING_SUBSCRIPTIONS,
+            userId: userId
+        }
     }
 });
 
--- webapp/static/jssrc/constants.js
+++ webapp/static/jssrc/constants.js
@@ -5,6 +5,8 @@ module.exports = {
         LOGGED_IN:null,
         LOGGED_OUT:null,
         LOGGING_IN:null,
-        PODCAST_FETCHED:null
+        PODCAST_FETCHED:null,
+        SUBSCRIPTIONS_FETCHED: null,
+        FETCHING_SUBSCRIPTIONS: null
     })
 }
