b501a0afc6202a9219cb6bc077d30ce7a7108c9b
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-21 15:43:04 +0200

    resolve conflict

--- webapp/static/jssrc/components/podcasts/subscribe-button.jsx
+++ webapp/static/jssrc/components/podcasts/subscribe-button.jsx
@@ -6,23 +6,49 @@ const SubscriptionsStore = require("../../stores/subscriptions-store");
 const Spinner = require("../common/spinner.jsx");
 
 const LoginButton = React.createClass({
+    mixins: [CurrentUserStore.mixin, SubscriptionsStore.mixin]
     render(){
-        if(!CurrentUserStore.getCurrentUser()) return;
-        if(!SubscriptiosStore.getSubscriptions("me")){
-            if(SubscriptionsStore.isFetchingSubscriptions("me")){
+        if(!this.state.user) return;
+        if(!this.state.subscriptions){
+            if(this.state.fetching){
                 return "<Spinner></Spinner>"
             }
             return;
         }
 
         var className = "button " + (this.props.className || "");
+        if(!subscribed){
+            return (
+                <a className={className} onClick={this.subscribe} >Subscribe</a>
+            )
+        }
+        className = "button bg-darken-2"
         return (
-            <a className={className} onClick={this.handleClick} >Subscribe</a>
+            <a className={className} onClick={this.unsubscribe}>Unsubscribe</a>
         )
+
     },
-    handleClick(e){
+    makeState(){
+        return {
+            user: CurrentUserStore.getCurrentUser(),
+            subscriptions: SubscriptiosStore.getSubscriptions("me"),
+            fetching: SubscriptionsStore.isFetchingSubscriptions("me"),
+            subscribed: SubscriptionsStore.isSubscribedTo("me", this.props.podcast)
+        }
+    },
+    getInitialState(){
+        return this.makeState()
+    }
+    subscribe(e){
         e.preventDefault();
-        //TODO implement subscribe action
+        PodcastsActions.subscribe(this.props.podcast);
+    },
+    unsubscribe(e){
+        e.preventDefault();
+        PodcastsActions.unsubscribe(this.props.podcast);
+    },
+    storeDidChange(){
+        this.setState(this.makeState());
     }
 });
 
