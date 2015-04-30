abe699c56a629c207a04249b5f05395603929451
Frederik Creemers <frederikcreemers@gmail.com>
2015-04-30 20:40:28 +0200

    dynamically set SERVER_HOST config variable.

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -1,7 +1,11 @@
 import json
 import os
+import logging
 
 data = json.load(open(os.path.join(os.path.dirname(__file__), "settings.json")))
 
 for key, value in data.iteritems():
     locals()[key] = value
+
+SERVER_NAME = os.environ["HTTP_HOST"]
+logging.error("SERVER_NAME: %s" % SERVER_NAME)
