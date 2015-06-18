28104a08ec81fae4a8f8c550261a2bdd8a87c55b
Frederik Creemers <frederikcreemers@gmail.com>
2015-06-18 17:00:19 +0200

    implement hsts in flask. Looking into doing it in uwsgi.

--- webapp/app.py
+++ webapp/app.py
@@ -7,12 +7,25 @@ import flask
 import flask_restful
 
 from config import settings
+from flask import request
+from flask import redirect
 
 app = flask.Flask(__name__)
 app.config.from_object(settings)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 db.init_db(app)
 cache.init_cache(app)
+
+@app.before_request
+def ensure_secure():
+    if not request.is_secure and "localhost" not in request.host:
+       redirect(request.url.replace("http", "https", 1), 301)
+
+@app.after_request
+def hsts(response):
+    if "localhost" not in request.host:
+        response.headers["Strict-Transport-Security"] = "max-age=31536000; preload"
+    return response
 
 with app.app_context():
     from users import users_blueprint
