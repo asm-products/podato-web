67f257d78458689b5e9c4ca1d2a8c3950e1bb6ec
Frederik Creemers <frederikcreemers@gmail.com>
2015-06-19 12:31:58 +0200

    rmmove hsts methods from flask app, as it is now handled by uwsgi.

--- webapp/app.py
+++ webapp/app.py
@@ -16,17 +16,6 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 db.init_db(app)
 cache.init_cache(app)
 
-@app.before_request
-def ensure_secure():
-    if not request.is_secure and "localhost" not in request.host:
-       redirect(request.url.replace("http", "https", 1), 301)
-
-@app.after_request
-def hsts(response):
-    if "localhost" not in request.host:
-        response.headers["Strict-Transport-Security"] = "max-age=31536000; preload"
-    return response
-
 with app.app_context():
     from users import users_blueprint
     from api import api_blueprint
